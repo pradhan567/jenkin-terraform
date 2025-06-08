348a1c60749298b9fba155bf7a37dd73c8af387c
manas <manaspradhan567@gmail.com>
2025-06-08 22:22:01 +0530

    updated code

--- ec2-creation/providers.tf
+++ ec2-creation/providers.tf
@@ -5,7 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
+
+backend "s3" {
+  bucket         = "terra-state-storage-bucket"
+  key            = "terraform/state"
+  region         = "ap-south-1"
+  use_lockfile = true
+    }
+  }
 
 # Configure the AWS Provider
 provider "aws" {
---
+++ ec2-creation/stop-jenkins.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "stop_jenkins_remote" {
+  triggers = {
+    instance_ip = aws_instance.terra.public_ip
+  }
+
+  provisioner "remote-exec" {
+    when   = destroy
+    inline = ["sudo docker stop jenkins || true"]
+
+    connection {
+      type        = "ssh"
+      host        = self.triggers.instance_ip
+      user        = "ec2-user"
+      private_key = file("/Users/manasranjanpradhan/Documents/manas-terraform-learning.pem")
+    }
+  }
+}
